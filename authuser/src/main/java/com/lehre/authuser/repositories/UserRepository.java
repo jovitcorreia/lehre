e812a998d64e1f90326ced15acd631e9eeb31e20
castanho <castanhocorreia.dev@gmail.com>
2021-10-24 02:18:16 -0300

    Add user repository interface

---
+++ authuser/src/main/java/com/lehre/authuser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lehre.authuser.repositories;
+
+import com.lehre.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {}
