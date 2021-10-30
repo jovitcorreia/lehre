55e14e5e8319990b32f727adbd234d8b566345f3
castanho <castanhocorreia.dev@gmail.com>
2021-10-30 10:45:25 -0300

    Add repositories and controllers for course and module

--- course/src/main/java/com/lehre/course/controller/CourseController.java
+++ course/src/main/java/com/lehre/course/controller/CourseController.java
@@ -1,2 +1,97 @@
-package com.lehre.course.controller;public class CourseController {
+package com.lehre.course.controller;
+
+import com.lehre.course.data.CourseData;
+import com.lehre.course.mapper.CourseMapper;
+import com.lehre.course.model.CourseModel;
+import com.lehre.course.service.CourseService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/courses")
+@RestController
+public class CourseController {
+  private final CourseService courseService;
+
+  @Autowired
+  public CourseController(CourseService courseService) {
+    this.courseService = courseService;
+  }
+
+  @PostMapping
+  public ResponseEntity<?> store(@RequestBody CourseData courseData) {
+    var courseModel = new CourseMapper().from(courseData);
+    courseService.store(courseModel);
+    courseModel.add(
+        linkTo(methodOn(CourseController.class).show(courseModel.getCourseId())).withSelfRel());
+    return ResponseEntity.status(HttpStatus.CREATED).body(courseModel);
+  }
+
+  @GetMapping("/{courseId}")
+  public ResponseEntity<?> show(@PathVariable UUID courseId) {
+    Optional<CourseModel> courseModelOptional = courseService.find(courseId);
+    if (courseModelOptional.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(String.format("Course with id %s not found!", courseId));
+    }
+    CourseModel courseModel = courseModelOptional.get();
+    courseModel.add(
+        linkTo(methodOn(CourseController.class).show(courseModel.getCourseId())).withSelfRel());
+    return ResponseEntity.status(HttpStatus.OK).body(courseModel);
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<CourseModel>> index(
+      @PageableDefault(sort = "creationDate", direction = Sort.Direction.ASC) Pageable pageable) {
+    Page<CourseModel> courseModelPage = courseService.index(pageable);
+    if (!courseModelPage.isEmpty()) {
+      for (CourseModel course : courseModelPage.toList()) {
+        course.add(linkTo(methodOn(CourseController.class).show(course.getCourseId())).withSelfRel());
+      }
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(courseModelPage);
+  }
+
+  @PutMapping("/{courseId}")
+  public ResponseEntity<?> update(
+      @PathVariable UUID courseId, @RequestBody @Validated CourseData courseData) {
+    Optional<CourseModel> courseModelOptional = courseService.show(courseId);
+    if (courseModelOptional.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(String.format("Course with id %s not found!", courseId));
+    }
+    CourseModel courseModel = courseModelOptional.get();
+    BeanUtils.copyProperties(courseData, courseModel);
+    courseService.store(courseModel);
+    courseModel.add(
+        linkTo(methodOn(CourseController.class).show(courseModel.getCourseId())).withSelfRel());
+    return ResponseEntity.status(HttpStatus.OK).body(courseModel);
+  }
+
+  @DeleteMapping("/{courseId}")
+  public ResponseEntity<?> delete(@PathVariable UUID courseId) {
+    Optional<CourseModel> courseModelOptional = courseService.show(courseId);
+    if (courseModelOptional.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(String.format("Course with id %s not found!", courseId));
+    }
+    CourseModel courseModel = courseModelOptional.get();
+    courseService.delete(courseModel);
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(String.format("Course  with id %s deleted  successfully!", courseId));
+  }
 }
--- course/src/main/java/com/lehre/course/controller/ModuleController.java
+++ course/src/main/java/com/lehre/course/controller/ModuleController.java
@@ -1,2 +1,77 @@
-package com.lehre.course.controller;public class ModuleController {
+package com.lehre.course.controller;
+
+import com.lehre.course.data.ModuleData;
+import com.lehre.course.model.CourseModel;
+import com.lehre.course.model.ModuleModel;
+import com.lehre.course.service.CourseService;
+import com.lehre.course.service.ModuleService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+import java.util.UUID;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+public class ModuleController {
+  private final CourseService courseService;
+  private final ModuleService moduleService;
+
+  @Autowired
+  public ModuleController(CourseService courseService, ModuleService moduleService) {
+    this.courseService = courseService;
+    this.moduleService = moduleService;
+  }
+
+  @PostMapping("/courses/{courseId}/modules/{moduleId}")
+  public ResponseEntity<?> store(
+      @PathVariable UUID courseId, @RequestBody @Valid ModuleData moduleData) {
+    Optional<CourseModel> courseModelOptional = courseService.find(courseId);
+    if (courseModelOptional.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(String.format("Course with id %s not found.", courseId));
+    }
+    CourseModel courseModel = courseModelOptional.get();
+    var moduleModel = new ModuleModel();
+    BeanUtils.copyProperties(moduleData, moduleModel);
+    moduleModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+    moduleModel.setCourse(courseModel);
+    moduleService.save(moduleModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(moduleModel);
+  }
+
+  @PutMapping("/courses/{courseId}/modules/{moduleId}")
+  public ResponseEntity<?> update(
+      @RequestBody ModuleData moduleData,
+      @PathVariable UUID courseId,
+      @PathVariable UUID moduleId) {
+    Optional<ModuleModel> moduleModelOptional =
+        moduleService.findModuleIntoCouse(courseId, moduleId);
+    if (moduleModelOptional.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course.");
+    }
+    ModuleModel moduleModel = moduleModelOptional.get();
+    BeanUtils.copyProperties(moduleData, moduleModel);
+    moduleService.save(moduleModel);
+    return ResponseEntity.status(HttpStatus.OK).body(moduleData);
+  }
+
+  @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
+  public ResponseEntity<?> delete(@PathVariable UUID courseId, @PathVariable UUID moduleId) {
+    Optional<ModuleModel> moduleModelOptional =
+        moduleService.findModuleIntoCouse(courseId, moduleId);
+    if (moduleModelOptional.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course.");
+    }
+    ModuleModel moduleModel = moduleModelOptional.get();
+    moduleService.delete(moduleModel);
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(String.format("Module with id %s deleted successfully.", moduleId));
+  }
 }
