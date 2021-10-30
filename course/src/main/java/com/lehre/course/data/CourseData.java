4015c71f067c9dcf1ac6333bb6f1f977c2453051
castanho <castanhocorreia.dev@gmail.com>
2021-10-30 10:44:22 -0300

    Add course and module data classes

--- course/src/main/java/com/lehre/course/data/CourseData.java
+++ course/src/main/java/com/lehre/course/data/CourseData.java
@@ -1,2 +1,26 @@
-package com.lehre.course.data;public class CourseData {
+package com.lehre.course.data;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.lehre.course.constant.CourseLevel;
+import com.lehre.course.constant.CourseStatus;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CourseData {
+  @NotBlank
+  private String name;
+  @NotBlank
+  private String description;
+  private String imageUrl;
+  @NotNull
+  private CourseStatus status;
+  @NotNull
+  private CourseLevel level;
+  @NotNull
+  private UUID instructor;
 }
--- course/src/main/java/com/lehre/course/data/ModuleData.java
+++ course/src/main/java/com/lehre/course/data/ModuleData.java
@@ -1,2 +1,13 @@
-package com.lehre.course.data;public class ModuleData {
+package com.lehre.course.data;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ModuleData {
+  @NotBlank private String title;
+  @NotBlank private String description;
 }
