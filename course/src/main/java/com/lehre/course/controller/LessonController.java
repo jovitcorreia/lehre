b73285b79e99c6bd5af873fc85edead95d7c66dc
castanho <castanhocorreia.dev@gmail.com>
2021-10-31 15:27:43 -0300

    Add specification-arg-resolver dependency

---
+++ course/src/main/java/com/lehre/course/controller/LessonController.java
@@ -0,0 +1,2 @@
+package com.lehre.course.controller;public class LessonController {
+}
---
+++ course/src/main/java/com/lehre/course/spec/SpecTemplate.java
@@ -0,0 +1,19 @@
+package com.lehre.authuser.spec;
+
+import com.lehre.authuser.model.UserModel;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SpecTemplate {
+  @And({
+    @Spec(path = "email", spec = Like.class),
+    @Spec(path = "type", spec = Equal.class),
+    @Spec(path = "status", spec = Equal.class),
+    @Spec(path = "username", spec = Like.class),
+    @Spec(path = "fullName", spec = Like.class)
+  })
+  public interface UserSpec extends Specification<UserModel> {}
+}
