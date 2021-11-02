4633bed471c7fabaef4601ea612a5c65c96fced2
castanho <castanhocorreia.dev@gmail.com>
2021-11-02 10:00:11 -0300

    Add ISO 8601 UTC datetime format

--- course/src/main/java/com/lehre/course/model/CourseModel.java
+++ course/src/main/java/com/lehre/course/model/CourseModel.java
@@ -45,11 +45,11 @@ public class CourseModel extends RepresentationModel<CourseModel> implements Ser
   private String imageUrl;
 
   @Column(nullable = false)
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
   private LocalDateTime creationDate;
 
   @Column(nullable = false)
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
   private LocalDateTime lastUpdateDate;
 
   @Column(nullable = false)
--- course/src/main/java/com/lehre/course/model/ModuleModel.java
+++ course/src/main/java/com/lehre/course/model/ModuleModel.java
@@ -39,7 +39,7 @@ public class ModuleModel implements Serializable {
   @Column(nullable = false)
   private String description;
 
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
   private LocalDateTime creationDate;
 
   @JoinColumn(name = "course_id")
