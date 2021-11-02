bac1e478469bbe8d3322e7521d3082294c446fcc
castanho <castanhocorreia.dev@gmail.com>
2021-11-02 10:19:19 -0300

    Enable ANSI ouput and remove default datetime config

--- authuser/src/main/java/com/lehre/authuser/config/DateTimeConfig.java
+++ authuser/src/main/java/com/lehre/authuser/config/DateTimeConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import java.time.format.DateTimeFormatter;
